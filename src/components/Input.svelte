369cc7497a00a7aea7f8c29e8dd0462350daeb3e
Lucas Lopes de Oliveira <lucas.lopes6588@gmail.com>
2023-03-30 22:07:25 -0300

    Finalizado criação de componentes e layout da tela de boas vindas.

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,2 +1,30 @@
-<script lang="ts"></script>
-<style lang="scss"></style>
+<script lang="ts">
+
+export let labelName: string;
+export let placeholderText: string;
+
+
+</script>
+
+<style lang="scss">
+
+$base-gray: gray;
+
+.label {
+  font-weight: 400;
+}
+
+.input {
+  
+  width: 100%;
+  height: 2.8rem;
+  padding: .5rem;
+  margin: .5rem 0;
+  border-radius: .8rem;
+  border: 1px solid $base-gray;
+}
+
+</style>
+
+<label class="label" for="name">{ labelName }</label>
+<input class="input" id="name" placeholder="{placeholderText}" type="text">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<script lang="ts"></script>
-<style lang="scss"></style>
+<script lang="ts">
+	import Button from "../components/Button.svelte";
+import FooterLogo from "../components/FooterLogo.svelte";
+	import Input from "../components/Input.svelte";
+
+</script>
+
+<style lang="scss">
+  @import '../styles/global.scss';
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 3rem;
+    height: 100vh;
+    width: 100vw;
+
+    .header {
+
+      &__title {
+        font-weight: 700;
+      }
+    }
+
+    .main {
+
+      &__title {
+        font-size: 1rem;
+        font-weight: 400;
+        margin: 2rem 0;
+      }
+    }
+
+  }
+
+
+</style>
+
+<section class="content">
+  <header class="header">
+    <h1 class="header__title">Car Assist</h1>
+    <h4 class="header__subtitle">Bem Vindo!</h4>
+    </header>
+
+    <main class="main">
+      <h5 class="main__title">Para iniciar digite seu nome.</h5>
+
+      <form class="form" action="">
+        <Input labelName="Nome" placeholderText="Digite seu nome" />
+        <Button buttonTitle="Acessar aplicativo" buttonIcon="bi-arrow-right" />
+      </form>
+    </main>
+
+    <FooterLogo />
+
+</section>
